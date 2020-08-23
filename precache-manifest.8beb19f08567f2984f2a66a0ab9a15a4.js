eb626dd039bbc39e7ba927f67a2f42422260a83f
Nandhini <42376766+Nandhini1405@users.noreply.github.com>
2020-08-23 21:31:30 +0530

    Add files via upload

---
+++ precache-manifest.8beb19f08567f2984f2a66a0ab9a15a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7969d96a2a29863136da5b85c912e9",
+    "url": "/Reactproject/index.html"
+  },
+  {
+    "revision": "9229e6bd9a0226822b11",
+    "url": "/Reactproject/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a97d77ebafb333f102ff",
+    "url": "/Reactproject/static/css/main.f55fd0a4.chunk.css"
+  },
+  {
+    "revision": "9229e6bd9a0226822b11",
+    "url": "/Reactproject/static/js/2.fd1d6a78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reactproject/static/js/2.fd1d6a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97d77ebafb333f102ff",
+    "url": "/Reactproject/static/js/main.94bf6ea9.chunk.js"
+  },
+  {
+    "revision": "7734aef4f851523ab7c2",
+    "url": "/Reactproject/static/js/runtime-main.c1c9a5c5.js"
+  },
+  {
+    "revision": "5ec90225bbb52b275b9b1cba7f808ef4",
+    "url": "/Reactproject/static/media/Nginx.5ec90225.png"
+  },
+  {
+    "revision": "5bb75c5c825bc2a9308cb2b34fb4c4ba",
+    "url": "/Reactproject/static/media/laravel.5bb75c5c.png"
+  },
+  {
+    "revision": "8fa3cc85837f13c58b9840f6e0b01944",
+    "url": "/Reactproject/static/media/linux.8fa3cc85.jpg"
+  }
+]);
